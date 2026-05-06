515379c68532c1417390d81ed970a2c8c22d8f82
Sabo <saleb2k@gmail.com>
2026-05-06 14:07:08 +0200

    feat(backend): Implemented basic authentication
    #4

---
+++ backend/src/main/java/com/example/petmatch/security/AdvertisementUserDetailsService.java
@@ -0,0 +1,21 @@
+package com.example.petmatch.security;
+
+import com.example.petmatch.domain.entities.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import com.example.petmatch.repositories.UserRepository;
+
+@RequiredArgsConstructor
+public class AdvertisementUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+        return new AdvertisementUserDetails(user);
+    }
+}
